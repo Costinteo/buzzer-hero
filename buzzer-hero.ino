25308569a32e26d45d6fb96341c13ede563482be
Costinteo <costinteo@gmail.com>
2021-12-14 23:26:28 +0200

    implemented a not-functioning menu linking

--- buzzer-hero.ino
+++ buzzer-hero.ino
@@ -1,11 +1,26 @@
 #include "src/util.hpp"
 #include "src/game.hpp"
 #include "src/joystick.hpp"
+#include "src/menu.hpp"
 
+/* MENU LAYOUTS */
+Button mainMenu[] = {
+  Button("Play",       ButtonType::enterMenu, constants::playMenuLayout),
+  Button("Options",    ButtonType::enterMenu, constants::optionsMenuLayout),
+  Button("About",      ButtonType::enterMenu, constants::aboutMenuLayout)
+};
+
+Button optionsMenu[] = {
+  Button("Back",       ButtonType::enterMenu, constants::mainMenuLayout),
+  Button("Contrast",   ButtonType::option),
+  Button("Brightness", ButtonType::option),
+  Button("Difficulty", ButtonType::option)
+};
 
 Game * const buzzerHero = Game::getInstance();
 
 void setup() {
+  util::linkMenuPointers(constants::LAYOUTS_NUM, nullptr, mainMenu, optionsMenu);
   buzzerHero->printMessage("Initiated game!");
   delay(1000);
   buzzerHero->clearRow(0);
